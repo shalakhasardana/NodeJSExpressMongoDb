ce50c18459b41bb655a6aa911f77eb40813f2b38
unknown <shalakhasardana18@gmail.com>
2020-10-14 20:05:49 -0700

    Add basic jarvascript code and run using  command node app

---
+++ app.js
@@ -0,0 +1,25 @@
+const rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+function solveRect(l, w) {
+  console.log(`Solving for rectange with dimensions: ${l}, ${w} `);
+
+  if (l <= 0 || w <= 0) {
+    console.log(
+      `Rectange dimensions must be grater than zero. Received : ${l} , ${w}`
+    );
+  } else {
+    console.log(`Area of rectangle: ${rect.area(l, w)}`);
+    console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
+  }
+}
+
+solveRect(2, 4);
+
+solveRect(3, 8);
+
+solveRect(0, 4);
+
+solveRect(2, -4);
